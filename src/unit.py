6f4b3aa8d6971afb8eda2902a9abad951c52e8c6
Davey <dwaite498@gmail.com>
2025-02-26 23:47:01 -0600

    Update Scout for mouse movement

--- src/unit.py
+++ src/unit.py
@@ -1,4 +1,4 @@
-from settings import TICK_RATE
+from settings import MOVE_TICK_RATE
 
 class Scout:
     def __init__(self, x, y):
@@ -6,25 +6,16 @@ class Scout:
         self.y = y
         self.last_move = 0
 
-    def move(self, direction, game):
+    def move_to(self, x, y, game):
         current_time = game.time.time()
-        if current_time - self.last_move < TICK_RATE:
+        if current_time - self.last_move < MOVE_TICK_RATE:
             return False
-        new_x, new_y = self.x, self.y
-        if direction == "north" and self.y > 0:
-            new_y -= 1
-        elif direction == "south" and self.y < game.planet["height"] - 1:
-            new_y += 1
-        elif direction == "west" and self.x > 0:
-            new_x -= 1
-        elif direction == "east" and self.x < game.planet["width"] - 1:
-            new_x += 1
-        else:
-            return False
-        self.x, self.y = new_x, new_y
-        self.last_move = current_time
-        self.reveal_tiles(game)
-        return True
+        if 0 <= x < game.planet["width"] and 0 <= y < game.planet["height"]:
+            self.x, self.y = x, y
+            self.last_move = current_time
+            self.reveal_tiles(game)
+            return True
+        return False
 
     def reveal_tiles(self, game):
         for dx in [-1, 0, 1]:
