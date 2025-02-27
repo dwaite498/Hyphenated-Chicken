c8ce5df0e1c126731c5ca7152fb6fb9f68ff78de
Davey <dwaite498@gmail.com>
2025-02-26 22:44:22 -0600

    Update settings for 10x10 and Pygame

--- src/settings.py
+++ src/settings.py
@@ -1,4 +1,6 @@
-MAP_WIDTH = 5
-MAP_HEIGHT = 5
+MAP_WIDTH = 10
+MAP_HEIGHT = 10
 TICK_RATE = 1  # Seconds per tick
 METAL_RATE = 1  # Metal per tick from colony
+WINDOW_SIZE = 600  # Pixels
+TILE_SIZE = WINDOW_SIZE // MAP_WIDTH  # 60 pixels
